2e1b9ce7b3e805c3fd298e01379042a9514b862c
rtmrshr <ratmir.sher@gmail.com>
2023-01-17 20:34:27 +0200

    Initial Commit

---
+++ MilestoneProject4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProject4-6
+//
+//  Created by  ratmir on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
